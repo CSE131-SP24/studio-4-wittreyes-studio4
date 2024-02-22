1c877dd8005442ccfb214db1b967dbe875e282a2
jay <jocel@172.27.70.226>
2024-02-22 11:06:54 -0600

    beautiful

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,42 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.black);
+		StdDraw.filledRectangle(.5,.5,.45,.3);
 		
+		StdDraw.setPenColor(Color.black);
+		StdDraw.rectangle(.5,.5,.45,.3);
+
+		StdDraw.setPenColor(Color.yellow);
+		StdDraw.filledCircle(.5,.5,.2);
+
+		StdDraw.setPenColor(Color.black);
+		double[] xs = {.5,.95,.95};
+		double[] ys = {.5,.8,.2};
+		StdDraw.filledPolygon(xs,ys);
+		
+		StdDraw.setPenColor(Color.black);
+		StdDraw.filledCircle(.5,.6,.02);
+		
+		StdDraw.setPenColor(Color.white);
+		StdDraw.filledCircle(.85,.5,.02);
+		
+		StdDraw.setPenColor(Color.white);
+		StdDraw.filledCircle(.75,.5,.02);
+		
+		StdDraw.setPenColor(Color.white);
+		StdDraw.filledCircle(.65,.5,.02);
+
+		StdDraw.setPenRadius(.01);
+		StdDraw.setPenColor(Color.blue);
+		StdDraw.rectangle(.5, .5, .45, .25);
+		
+		StdDraw.setPenColor(Color.blue);
+		StdDraw.rectangle(.5, .5, .45, .275);
+		
+		StdDraw.setPenRadius(0.01);
+		StdDraw.setPenColor(Color.black);
+		StdDraw.rectangle(.5,.5,.45,.3);
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,55 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		if(shapeType.equals("rectangle")==true||shapeType.equals("ellipse")==true) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+
+			if(shapeType.equals("rectangle")==true) {
+				if(isFilled == true) {
+					StdDraw.filledRectangle(x,y,halfWidth,halfHeight);
+				}
+				else {
+					StdDraw.rectangle(x,y,halfWidth,halfHeight);
+				}
+			}
+			if(shapeType.equals("ellipse")==true) {
+				if(isFilled == true) {
+					StdDraw.filledEllipse(x,y,halfWidth,halfHeight);
+				}
+				else {
+					StdDraw.ellipse(x,y,halfWidth,halfHeight);
+				}
+
+
+			}
+		}
+		else {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+
+			double[] xs = {x1,x2,x3};
+			double[] ys = {y1,y2,y3};
+			if(isFilled==true) {
+				StdDraw.filledPolygon(xs, ys);
+			}
+			else {
+				StdDraw.polygon(xs, ys);
+			}
+		}
 	}
 }
